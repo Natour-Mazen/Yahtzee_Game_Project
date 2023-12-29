6af193c6a11d3bc7fd2654302c9ecefb3c1abade
Léo Hervouet <leoohervouet@gmail.com>
2023-12-29 17:21:20 +0100

    Add comments to the Figure.h

--- Yahtzee_Game_Lib/figure/Figure.h
+++ Yahtzee_Game_Lib/figure/Figure.h
@@ -8,10 +8,29 @@ class Figure
 {
 public:
     virtual ~Figure() {}
+
+    /** Calculate the score for this figure with the current dices.
+    *   @param diceValues : dices to calculate the score.
+    *   @return : the score.
+    **/
     virtual int calculateScore(const std::vector<int>& diceValues) = 0;
+
+    /** Give the description of the Brelan.
+    *   @return : the string of the description.
+    **/
     virtual std::string getName() const = 0;
+
+    /** Give the Id of this class.
+    *   @return : the id of the class
+    **/
     virtual short getId() const = 0;
+
+    /** TODO
+    **/
     virtual void serialize(std::ostream& out) const = 0;
+    
+    /** TODO
+    **/
     virtual void deserialize(std::istream& in) = 0;
 };
 
