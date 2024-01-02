87ab7866f95ac81eeb9baaf4ec45f277e4121354
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-01-02 22:13:33 +0100

    add comments to GrandSuite && Number figures

--- Yahtzee_Game_Lib/figure/figures/GrandeSuite.h
+++ Yahtzee_Game_Lib/figure/figures/GrandeSuite.h
@@ -1,10 +1,23 @@
 #pragma once
 #include "Figure.h"
 
-template<short ID>
+/**
+ * @brief Represents the "Grande Suite" figure in the Yahtzee game.
+ * @tparam ID The unique identifier for the Grande Suite figure.
+ */
+template <short ID>
 class GrandeSuite : public Figure {
 public:
-    int calculateScore(const std::vector<int>& diceValues) {
+    /**
+     * @brief Calculates the score for the Grande Suite figure based on the given dice values.
+     *
+     * This method calculates the score for a Grande Suite figure, where the score is 40 if the dice values
+     * form a sequence of five consecutive numbers, and 0 otherwise.
+     *
+     * @param diceValues The values of the dice.
+     * @return The calculated score for the Grande Suite figure.
+     */
+    int calculateScore(const std::vector<int>& diceValues)  {
         std::vector<int> sortedValues(diceValues);
         std::sort(sortedValues.begin(), sortedValues.end());
 
@@ -16,17 +29,27 @@ public:
         return 40;
     }
 
-    std::string getName() const {
+    /**
+     * @brief Gets the name of the Grande Suite figure.
+     * @return The name of the figure.
+     */
+    std::string getName() const  {
         return "Grande Suite";
     }
 
-    short getId() const {
+    /**
+     * @brief Gets the unique identifier of the Grande Suite figure.
+     * @return The unique identifier.
+     */
+    short getId() const  {
         return ID;
     }
 
-    void serialize(std::ostream& out) const {
+    /**
+     * @brief Serializes the Grande Suite figure to an output stream.
+     * @param out The output stream to serialize the data to.
+     */
+    void serialize(std::ostream& out) const  {
         out << "id: " << ID << "\n";
     }
 };
-
-
--- Yahtzee_Game_Lib/figure/figures/Number.h
+++ Yahtzee_Game_Lib/figure/figures/Number.h
@@ -1,13 +1,29 @@
 #pragma once
 #include "Figure.h"
 
-template<short ID>
+/**
+ * @brief Represents a generic "Number" figure in the Yahtzee game.
+ * @tparam ID The unique identifier for the Number figure.
+ */
+template <short ID>
 class Number : public Figure {
 public:
-    Number() = delete;
+    /**
+     * @brief Constructs a Number figure with the specified number.
+     * @param number The number associated with the figure.
+     */
     Number(unsigned int number) : m_number(number) {}
 
-    int calculateScore(const std::vector<int>& diceValues) {
+    /**
+     * @brief Calculates the score for the Number figure based on the given dice values.
+     *
+     * This method calculates the score for a Number figure, where the score is the sum of the dice values
+     * that match the specified number.
+     *
+     * @param diceValues The values of the dice.
+     * @return The calculated score for the Number figure.
+     */
+    int calculateScore(const std::vector<int>& diceValues)  {
         int score = 0;
         for (int value : diceValues) {
             if (value == m_number) {
@@ -17,20 +33,30 @@ public:
         return score;
     }
 
-    std::string getName() const {
+    /**
+     * @brief Gets the name of the Number figure.
+     * @return The name of the figure.
+     */
+    std::string getName() const  {
         return "Number " + std::to_string(m_number);
     }
 
-    short getId() const {
+    /**
+     * @brief Gets the unique identifier of the Number figure.
+     * @return The unique identifier.
+     */
+    short getId() const  {
         return ID;
     }
 
-    void serialize(std::ostream& out) const {
+    /**
+     * @brief Serializes the Number figure to an output stream.
+     * @param out The output stream to serialize the data to.
+     */
+    void serialize(std::ostream& out) const  {
         out << "id: " << ID << "\n";
     }
 
 private:
-    unsigned int m_number;
+    unsigned int m_number;  ///< The number associated with the Number figure.
 };
-
-
