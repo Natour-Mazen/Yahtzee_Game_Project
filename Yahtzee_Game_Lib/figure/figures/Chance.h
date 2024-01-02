3db9002e2ac24caace30d50715ddc0af9850814d
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-01-02 22:10:52 +0100

    add comments to Chance && Full figures

--- Yahtzee_Game_Lib/figure/figures/Chance.h
+++ Yahtzee_Game_Lib/figure/figures/Chance.h
@@ -1,10 +1,22 @@
 #pragma once
 #include "Figure.h"
 
-template<short ID>
+/**
+ * @brief Represents the "Chance" figure in the Yahtzee game.
+ * @tparam ID The unique identifier for the Chance figure.
+ */
+template <short ID>
 class Chance : public Figure {
 public:
-    int calculateScore(const std::vector<int>& diceValues) {
+    /**
+     * @brief Calculates the score for the Chance figure based on the given dice values.
+     *
+     * This method calculates the score by summing up all the values of the dice.
+     *
+     * @param diceValues The values of the dice.
+     * @return The calculated score for the Chance figure.
+     */
+    int calculateScore(const std::vector<int>& diceValues)  {
         int sum = 0;
         for (int value : diceValues) {
             sum += value;
@@ -12,16 +24,27 @@ public:
         return sum;
     }
 
-    std::string getName() const {
+    /**
+     * @brief Gets the name of the Chance figure.
+     * @return The name of the figure.
+     */
+    std::string getName() const  {
         return "Chance";
     }
 
-    short getId() const {
+    /**
+     * @brief Gets the unique identifier of the Chance figure.
+     * @return The unique identifier.
+     */
+    short getId() const  {
         return ID;
     }
 
-    void serialize(std::ostream& out) const {
+    /**
+     * @brief Serializes the Chance figure to an output stream.
+     * @param out The output stream to serialize the data to.
+     */
+    void serialize(std::ostream& out) const  {
         out << "id: " << ID << "\n";
     }
 };
-
--- Yahtzee_Game_Lib/figure/figures/Full.h
+++ Yahtzee_Game_Lib/figure/figures/Full.h
@@ -1,18 +1,29 @@
 #pragma once
 #include "Figure.h"
 
-template<short ID>
+/**
+ * @brief Represents the "Full" figure in the Yahtzee game.
+ * @tparam ID The unique identifier for the Full figure.
+ */
+template <short ID>
 class Full : public Figure {
 public:
-    int calculateScore(const std::vector<int>& diceValues) {
-        for (int i = 1; i <= 6; ++i)
-        {
+    /**
+     * @brief Calculates the score for the Full figure based on the given dice values.
+     *
+     * This method calculates the score for a Full figure, where there are two dice with one value
+     * and three dice with another value.
+     *
+     * @param diceValues The values of the dice.
+     * @return The calculated score for the Full figure.
+     */
+    int calculateScore(const std::vector<int>& diceValues)  {
+        for (int i = 1; i <= 6; ++i) {
             int count1 = static_cast<int>(std::count(diceValues.begin(), diceValues.end(), i));
             if (count1 == 2 || count1 == 3) {
-                for (int y = i + 1; y <= 6; ++y)
-                {
+                for (int y = i + 1; y <= 6; ++y) {
                     int count2 = static_cast<int>(std::count(diceValues.begin(), diceValues.end(), y));
-                    if (count1 == 2 && count2 == 3 || count1 == 3 && count2 == 2) {
+                    if ((count1 == 2 && count2 == 3) || (count1 == 3 && count2 == 2)) {
                         m_value1 = i;
                         m_value2 = y;
                         return 25;
@@ -23,22 +34,34 @@ public:
         return 0;
     }
 
-    std::string getName() const {
+    /**
+     * @brief Gets the name of the Full figure.
+     * @return The name of the figure.
+     */
+    std::string getName() const  {
         if (m_value1 == 0) {
             return "Full";
         }
         return "Full de " + std::to_string(m_value1) + " et " + std::to_string(m_value2);
     }
 
-    short getId() const {
+    /**
+     * @brief Gets the unique identifier of the Full figure.
+     * @return The unique identifier.
+     */
+    short getId() const  {
         return ID;
     }
 
-    void serialize(std::ostream& out) const {
+    /**
+     * @brief Serializes the Full figure to an output stream.
+     * @param out The output stream to serialize the data to.
+     */
+    void serialize(std::ostream& out) const  {
         out << "id: " << ID << "\n";
     }
 
 private:
-    int m_value1 = 0;
-    int m_value2 = 0;
+    int m_value1 = 0;  ///< The first value for the Full figure.
+    int m_value2 = 0;  ///< The second value for the Full figure.
 };
